7e199e1115a11e82fea10b2e504d0529327e7dd8
AsherKingpin <120887356+AsherKingpin@users.noreply.github.com>
2023-07-26 23:04:05 +0530

    Create datatypes.c

---
+++ Assignment 2/datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int a;
+	float b;
+	char string[] = "hello world";
+	a = 5;
+	b = 7.400;
+	printf("%d it is an integer!\n",a);
+	printf("%.3f it is a float!\n", b);
+	printf("%s it is a char!\n",string);
